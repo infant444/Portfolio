0d366ebcc5b585a3fd1813141bc7fa1133482ce2
infant04 <infant0467@gmail.com>
2024-06-20 19:37:15 +0900

    x

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,7 +28,7 @@ app.use("/api/message",messagerouter);
 
 app.use(express.static('public'))
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
+    res.sendFile(path.join(__dirname,'public','browser','index.html'))
 })
 
 const PORT=process.env.PORT||5000;
